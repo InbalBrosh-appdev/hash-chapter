a1735f47c81f69de5fcbd2d61f13842dc84ae249
inbalbrosh <ibrosh@chicagobooth.edu>
2022-01-22 21:42:00 +0000

    hash homework

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -7,3 +7,18 @@
 sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+
+entered_number = gets.chop.to_i
+keys = sample_hash.keys
+was_found = false
+
+keys.each do |key|
+  if entered_number == sample_hash[key]
+    p "#{entered_number} is under the key: #{key}"
+    was_found = true
+  end
+end
+
+if !was_found
+  p "Could not find the integer #{entered_number}"
+end
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,9 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
+
 p "Enter a name, age, and occupation separated by spaces:"
+
+person = gets.chop.split
+
+person = {:name => person[0], :age => person[1].to_i, :occupation => person[2]}
+
+p person
